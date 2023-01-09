35a3a760ffed9fd81c9c06d2c18b8f609e3bd6e5
BB-Simon <simon.chowdery@gmail.com>
2023-01-09 17:33:31 +0200

    Add add & remove book functions

--- js/index.js
+++ js/index.js
@@ -1,12 +1,42 @@
 const addBookForm = document.getElementById("add-book");
+const bookList = document.getElementById("book-list");
+const books = [];
+
+
+function addBookToTheList(book, cb) {
+  const li = document.createElement("li");
+  li.classList.add('list-item');
+  li.innerHTML = `
+    <h3>${book.title}</h3>
+    <h4>${book.author}</h4>
+    <button id="remove-book" data-id=${book.id}>Remove</button>
+    `;
+
+  bookList.appendChild(li)
+  
+  const removeBnt = document.getElementById("remove-book");
+  removeBnt.addEventListener('click', cb())
+}
+
+function removeBookFromTheList(){
+  books.filter(book => book.title !== title)
+  books.forEach((book) => addBookToTheList(book))
+}
+
+
 
 function handleSubmit(e) {
   e.preventDefault();
-  console.log(document.forms["add-book"]);
 
   const bookTitle = addBookForm['title'].value;
   const bookAuthor = addBookForm['author'].value;
-  console.log(bookTitle, bookAuthor);
+  books.push({
+    id: 'aa',
+    title: bookTitle,
+    author: bookAuthor
+  })
+  books.forEach((book) => addBookToTheList(book, removeBookFromTheList))
 }
+
 
 addBookForm.addEventListener("submit", handleSubmit);
